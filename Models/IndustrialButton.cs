948464bc7c0753fafd702a503220be358093cde4
Kaiwoknats <59572199+Kaiwoknats@users.noreply.github.com>
2023-12-18 23:35:08 +0100

    Initial Commit

---
+++ Models/IndustrialButton.cs
@@ -0,0 +1,8 @@
+﻿namespace AltV.Atlas.Scaleforms.Shared.Models;
+
+public record IndustrialButton
+{
+    public int IntParam { get; set; } = -1;
+    public string? InstructionalButtonsString { get; set; }
+    public string? DisplayText { get; set; }
+}
---
+++ ScaleformConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace AltV.Atlas.Scaleforms.Shared;
+
+/// <summary>
+/// A static class containing all scaleform related constants
+/// </summary>
+public static class ScaleformConstants
+{
+    /// <summary>
+    /// The value for the drawIndustrialMenu event
+    /// </summary>
+    public const string DrawIndustrialMenuEventName = "atlas:scaleforms:drawIndustrialMenu";
+    
+    /// <summary>
+    /// The value for the hideIndustrialMenu event
+    /// </summary>
+    public const string HideIndustrialMenuEventName = "atlas:scaleforms:hideIndustrialMenu";
+}
